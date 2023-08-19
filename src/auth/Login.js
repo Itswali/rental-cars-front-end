b7f81e35d80da3c03ba1d47b616a9d091dbbf64b
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-08-19 15:59:26 +0530

    update eslint config

--- src/auth/Login.js
+++ src/auth/Login.js
@@ -27,7 +27,7 @@ export default function Login() {
     };
 
     try {
-      const response = await fetch('http://localhost:3000/api/v1/login', {
+      const response = await fetch('http://localhost:3001/api/v1/login', {
         method: 'POST',
         headers,
         body: JSON.stringify(formData),
--- src/components/NavigationPanel.js
+++ src/components/NavigationPanel.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 
 const NavigationPanel = () => {
+  // eslint-disable-next-line no-unused-vars
   const [navigationLinks, setNavigationLinks] = useState([]);
 
   useEffect(() => {
-    fetch('http://127.0.0.1:3000//api/v1/navigation_links')
+    fetch('http://127.0.0.1:3001/api/v1/navigation_links')
       .then((response) => response.json())
       .then((data) => setNavigationLinks(data));
   }, []);
@@ -12,11 +13,13 @@ const NavigationPanel = () => {
   return (
     <nav>
       <ul>
-        {navigationLinks.map((link) => (
+        { /*
+          navigationLinks?.map((link) => (
           <li key={link.name}>
             <a href={link.link}>{link.name}</a>
           </li>
-        ))}
+        ))
+        */ }
       </ul>
     </nav>
   );
