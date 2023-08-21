a5909f542c9cfe243d2734a04a3a78f5c7e46618
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-08-21 20:29:52 +0530

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 import './App.css';
-import React, { useEffect } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Splash from './components/Splash';
 import Registration from './auth/Registration';
 import Login from './auth/Login';
 import Home from './components/Home';
-import { AuthProvider } from './auth/AuthContext';
+import { useAuth } from './auth/AuthContext';
 
 function App() {
-  useEffect(() => {
+  const { authenticated } = useAuth();
 
-  }, []);
   return (
     <div className="App">
-      <AuthProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route exact path="/" element={<Splash />} />
-            <Route exact path="/registration" element={<Registration />} />
-            <Route exact path="/login" element={<Login />} />
-            <Route exact path="/home" element={<Home />} />
-          </Routes>
-        </BrowserRouter>
-      </AuthProvider>
+      <Routes>
+        <Route exact path="/" element={<Splash />} />
+        <Route exact path="/registration" element={<Registration />} />
+        <Route exact path="/login" element={<Login />} />
+
+        {
+                      authenticated ? (
+                        <Route exact path="/*" element={<Home />} />
+                      ) : (
+                        <Route exact path="*" element={<Splash />} />
+                      )
+}
+      </Routes>
     </div>
   );
 }
