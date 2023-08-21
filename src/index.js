afacc507fbaf802d6d183dd234cd18a4a436537b
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-08-21 20:30:54 +0530

    updated index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import './index.css';
+import { AuthProvider } from './auth/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <Router>
+        <App />
+      </Router>
+    </AuthProvider>
   </React.StrictMode>,
 );
