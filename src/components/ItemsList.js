abf517dcccce81b2f9d58d99b2a5b455c408dd96
Itswali <luckywali0986@gmail.com>
2023-08-21 21:30:02 +0500

    auth

--- src/components/ItemsList.js
+++ src/components/ItemsList.js
@@ -1,29 +1,74 @@
-// src/components/ItemsList.js
-import React, { useState, useEffect } from 'react';
+/* eslint-disable no-console */
+import React, { useState } from 'react';
+import { useAuth } from '../auth/AuthContext';
 
-const ItemsList = () => {
-  const [items, setItems] = useState([]);
+export default function Registration() {
+  const { authToken } = useAuth(); // Retrieve authToken from the auth context
+  const [formData, setFormData] = useState({
+    title: '',
+    description: '',
+    image_url: '',
+  });
 
-  useEffect(() => {
-    fetch('http://127.0.0.1:3000/api/v1/items')
-      .then((response) => response.json())
-      .then((data) => setItems(data)); // Assuming the data is an array of items
-  }, []);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const headers = {
+      Authorization: `Bearer ${authToken}`, // Include the authToken in the headers
+      'Content-Type': 'application/json',
+    };
+
+    try {
+      const response = await fetch('http://localhost:3000/api/v1/items', {
+        method: 'POST',
+        headers,
+        body: JSON.stringify({ item: formData }), // Send the form data as JSON
+      });
+
+      if (response.ok) {
+        // Handle success (e.g., show a success message or navigate to a success page)
+      } else {
+        const errorData = await response.json();
+        console.error('Error creating item:', errorData);
+      }
+    } catch (error) {
+      console.error('Error creating item:', error);
+    }
+  };
 
   return (
-    <div>
-      <h2>Items List</h2>
-      <ul>
-        {items.map((item) => (
-          <li key={item.id}>
-            <h3>{item.title}</h3>
-            <p>{item.description}</p>
-            <img src={item.image_url} alt={item.title} />
-          </li>
-        ))}
-      </ul>
-    </div>
+    <form className="item-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="title"
+        value={formData.title}
+        placeholder="Title"
+        required
+        onChange={handleChange}
+      />
+      <textarea
+        name="description"
+        value={formData.description}
+        placeholder="Description"
+        required
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        name="image_url"
+        value={formData.image_url}
+        placeholder="Image URL"
+        required
+        onChange={handleChange}
+      />
+      <button type="submit" className="create-button">Create Item</button>
+    </form>
   );
-};
-
-export default ItemsList;
+}
