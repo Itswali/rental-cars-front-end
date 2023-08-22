d6ada6ee5be7f91159743eb67c30d31b3d5fb0e3
Itswali <luckywali0986@gmail.com>
2023-08-22 16:26:00 +0500

    add delete function

--- src/components/ItemsList.js
+++ src/components/ItemsList.js
@@ -1,74 +1,34 @@
-/* eslint-disable no-console */
-import React, { useState } from 'react';
-import { useAuth } from '../auth/AuthContext';
+import React, { useState, useEffect } from 'react';
+import DeleteItemButton from './DeleteItemButton';
 
-export default function Registration() {
-  const { authToken } = useAuth(); // Retrieve authToken from the auth context
-  const [formData, setFormData] = useState({
-    title: '',
-    description: '',
-    image_url: '',
-  });
+const ItemsList = () => {
+  const [items, setItems] = useState([]);
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-  };
+  useEffect(() => {
+    fetch('http://127.0.0.1:3000/api/v1/items')
+      .then((response) => response.json())
+      .then((data) => setItems(data.data));
+  }, []);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const headers = {
-      Authorization: `Bearer ${authToken}`, // Include the authToken in the headers
-      'Content-Type': 'application/json',
-    };
-
-    try {
-      const response = await fetch('http://localhost:3000/api/v1/items', {
-        method: 'POST',
-        headers,
-        body: JSON.stringify({ item: formData }), // Send the form data as JSON
-      });
-
-      if (response.ok) {
-        // Handle success (e.g., show a success message or navigate to a success page)
-      } else {
-        const errorData = await response.json();
-        console.error('Error creating item:', errorData);
-      }
-    } catch (error) {
-      console.error('Error creating item:', error);
-    }
+  const handleDelete = (deletedItemId) => {
+    setItems(items.filter((item) => item.id !== deletedItemId));
   };
 
   return (
-    <form className="item-form" onSubmit={handleSubmit}>
-      <input
-        type="text"
-        name="title"
-        value={formData.title}
-        placeholder="Title"
-        required
-        onChange={handleChange}
-      />
-      <textarea
-        name="description"
-        value={formData.description}
-        placeholder="Description"
-        required
-        onChange={handleChange}
-      />
-      <input
-        type="text"
-        name="image_url"
-        value={formData.image_url}
-        placeholder="Image URL"
-        required
-        onChange={handleChange}
-      />
-      <button type="submit" className="create-button">Create Item</button>
-    </form>
+    <div>
+      <h2>Items List</h2>
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>
+            <h3>{item.attributes.title}</h3>
+            <p>{item.attributes.description}</p>
+            <img src={item.attributes.image_url} alt={item.attributes.title} />
+            <DeleteItemButton itemId={item.id} onDelete={() => handleDelete(item.id)} />
+          </li>
+        ))}
+      </ul>
+    </div>
   );
-}
+};
+
+export default ItemsList;
