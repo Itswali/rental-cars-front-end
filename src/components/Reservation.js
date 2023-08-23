9815bfbcbb5d6c8c13dd32d1b5b651232b954a76
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-08-23 08:38:01 +0000

    Add 'Reserve' component

---
+++ src/components/Reservation.js
@@ -0,0 +1,14 @@
+// import React from 'react';
+
+// const Reservation = () => {
+//   const hello = 'hello';
+//   return (
+//     <div>
+//       Reservations page.
+//       {' '}
+//       { hello }
+//     </div>
+//   );
+// };
+
+// export default Reservation;
---
+++ src/components/Reserve.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Reserve = () => {
+  const hello = 'hello';
+  return (
+    <div>
+      Reseve a car.
+      {' '}
+      { hello }
+    </div>
+  );
+};
+
+export default Reserve;
