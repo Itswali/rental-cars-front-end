cffdbeb4bd495842fddeabf422e48688c47b75f7
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-08-25 12:18:17 +0000

    Remove test borders

--- src/App.js
+++ src/App.js
@@ -31,18 +31,15 @@ function App() {
             <Route path="reserve" element={<Reserve />} />
             <Route path="reserve/:carParam" element={<Reserve />} />
             <Route path="my_reservations" element={<Reservation />} />
-            {/* <Route path="add_item" element={<h2>Add an item</h2>} />
-            <Route path="reserve" element={<h2>reserve an item</h2>} />
-            <Route path="reserve/:carParam" element={<h2>still reserve an item</h2>} />
-            <Route path="my_reservations" element={<h2>reservations</h2>} /> */}
           </Route>
+
           {
-              authenticated ? (
-                <Route path="/*" element={<Home />} />
-              ) : (
-                <Route path="*" element={<Splash />} />
-              )
-            }
+            authenticated ? (
+              <Route path="/*" element={<Home />} />
+            ) : (
+              <Route path="*" element={<Splash />} />
+            )
+          }
 
           <Route exact path="/" element={<Splash />} />
 
