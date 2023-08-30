dd2ffa2b0a597ba2350d5b0fa13f219378b784fc
Jonah Wambua <djmrjay@gmail.com>
2023-08-30 16:05:50 +0300

    Add mobile navbar

--- src/components/MobileNavigationBar.js
+++ src/components/MobileNavigationBar.js
@@ -1,22 +1,14 @@
-import React, { useState } from 'react';
-import NavigationPanel from './NavigationPanel';
+import React from 'react';
 
 const MobileNavigationBar = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
-
   return (
-    <div className={`mobile-navigation ${menuOpen ? 'open' : ''}`}>
-      <button className="hamburger-button" onClick={toggleMenu} type="button">
+    <div className="mobile-nav">
+      <button className="hamburger-button" type="button">
         <i className="bi bi-list"></i>
       </button>
-      {menuOpen && <NavigationPanel closeMenu={() => setMenuOpen(false)} />}
+      Test Data
     </div>
   );
 };
 
 export default MobileNavigationBar;
-
