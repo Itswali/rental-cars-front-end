4a6ffec9b1ad4129766401a8734f1890e44f2583
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-08-31 18:35:38 +0530

    updated splash screen

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default function Splash() {
-  return (
-    <div className="splash">
-      <img src="super-wheels-white-logo.svg" alt="logo" />
-      <Link to="/registration" className="credentials">
-        Sign Up
-      </Link>
-      <Link to="/login" className="credentials">
-        Log In
-      </Link>
+const Splash = () => (
+  <div className="splash">
+    <img src="super-wheels-white-logo.svg" alt="logo" />
+    <Link to="/registration" className="credentials">
+      Sign Up
+    </Link>
+    <Link to="/login" className="credentials">
+      Log In
+    </Link>
 
-    </div>
-  );
-}
+  </div>
+);
+export default Splash;
