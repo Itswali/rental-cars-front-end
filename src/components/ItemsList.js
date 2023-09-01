d7facb3f690972264aad4863837f232ed8b62791
Itswali <luckywali0986@gmail.com>
2023-09-01 10:44:03 +0500

    remove unsed comments

--- src/components/ItemsList.js
+++ src/components/ItemsList.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom'; // Import Link from React Router
+import { Link } from 'react-router-dom';
 
 const ItemsList = () => {
   const [items, setItems] = useState([]);
