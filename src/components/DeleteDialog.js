d4362cb4f910a1cad46cb68cef64e3a70a526abb
Itswali <luckywali0986@gmail.com>
2023-09-01 10:39:29 +0500

    remove no-comment-text nodes

--- src/components/DeleteDialog.js
+++ src/components/DeleteDialog.js
@@ -1,5 +1,4 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
-/* eslint-disable react/jsx-no-comment-textnodes */
 /* eslint-disable react/prop-types */
 /* eslint-disable no-console */
 import React, { useState, useEffect } from 'react';
